d138236016830b3b228001927f181a3328da36e5
katrijylkkae <katri.jylkka@gmail.com>
2022-05-19 13:24:06 +0300

    changes made in dom.js + style.scss

--- dom.js
+++ dom.js
@@ -1,6 +1,19 @@
 //Dom-manipulointi, eventit ja Rest-Api
 
-// Tässä on muo
+
+// Otsikon tekstiä muokataan. Queryselectorilla, voi valita myös elementtejä (ei tarvitse aina olla class tai id) 
+
+let title = "Tällä sivulla olen harjoitellut SASSsia ja DOMia"
+ let  h1Title = document.querySelector("h1");
+h1Title.innerHTML = title; 
+
+// Tässä kaikkien col-sm-12 -elementtien värit muutetaan. 
+
+let columns = document.querySelectorAll(".col-sm-12");
+columns.forEach( element => {
+  element.style.backgroundColor = "rgb(10, 56, 10)"
+});
+
 //Tässä luodaan uusi li-elementti listaan ja poistetaan ensimmäinen elementti
 
 let ul = document.querySelector("#list");
@@ -9,12 +22,39 @@ li.textContent = "Kuumakivihoitaja"
 ul.appendChild(li)
 ul.firstElementChild.remove();
 
+// Tässä muokataan sivuston alareunassa olevan "tekijä- tekstin tyylejä: kokoa, kirjoitusasua ja poistan text-decorationit
+
+document.getElementById("maker").style.fontSize = "large";
+document.getElementById("maker").style.fontFamily = "Arial";
+document.getElementById("maker").style.textDecoration = "none";
+document.getElementById("maker").style.color = "green";
+
+// Tässä muokattu navbarin nappia
+
+document.getElementById("special").style.paddingRight = "50px";
+document.getElementById("special").style.paddingLeft = "50px";
+document.getElementById("special").style.backgroundColor = "rgb(211, 84, 49)"
+
+// Tässä manipuloidaan classin nimeä.
+
+let div = document.getElementById("article");
+div.className = "manipulated"
+div.classList.add = "class"  // Class on nyt "manipulated class"
+div.classList.remove("manipulated") // Class on nyt nimeltään "class"
 
 
-// Tässä muokattu tyylejä  
-//
-//
-//
+// Tässä luon sivuston alareunaan uuden elementin, mikä pitää sisällään tekstiä.
+
+const body = document.body;
+const strong = document.createElement("strong");
+strong.innerText = "Tällä sivulla harjoitellaan Javascriptiä ja cssää." ; 
+body.append(strong);
+
+// Tässä luon hr -elementin sivulle 
+
+let something = document.createElement("hr");
+document.getElementById("insert").appendChild(something);
+
 
 // Tässä on käytössä RandomFox API (Rest)
 
@@ -26,13 +66,11 @@ data => document.getElementById('print-here').innerHTML
   = '<img src="' + data.image + '"/>'
   );
 
-
-
 // Eventtejä //Eventtejä // Eventtejä //Eventtejä //Eventtejä //Eventtejä //Eventtejä
 
   // Tässä eventissä tulee alert, kun napin päälle viedään hiiri
 
- let button = document.getElementById("try")
+ var button = document.getElementById("try")
 
  button.addEventListener("mouseover", tryFunction);
  
@@ -40,6 +78,7 @@ data => document.getElementById('print-here').innerHTML
   alert("Paina ()-nappi pohjaan saadaksesi eventin aikaan"), (false);
 
  }
+
 
  // Tässä näkee milloin sivustoa on viimeks muokattu
 
@@ -54,21 +93,22 @@ function myFunction() {
     openCv.document.close();
   }
 
-//tässä muutettu taustan väriä napin painalluksella
-
-let button2 = document.getElementById("button2")
-button2.addEventListener("click", myColorFunction)
-
- function myColorFunction() {
-    document.getElementById("background").style.backgroundColor = "rgb(10, 56, 10)";
-  }  
-
-
- // Kun footeria klikkaa muuttuu 
+ // Kun footeria klikkaa muuttuu teksti
 let footer = document.getElementById("text");
 
 footer.addEventListener("click", myFooterFunction)
 
-function myFooterFunction() {
-  document.getElementById("footer").innerHTML = " <p>KIITOS KUN ASIOIT MEILLÄ! <br> NOTKEUDESSA NÄHDÄÄN!</p>";
-}
+  function myFooterFunction() {
+    document.getElementById("footer").innerHTML = " <p>KIITOS KUN ASIOIT MEILLÄ! <br> NOTKEUDESSA NÄHDÄÄN!</p>";
+  }
+
+//tässä muutettu taustan väriä napin painalluksella
+
+let button2 = document.querySelector("#button2")
+button2.addEventListener("click", myColorFunction)
+
+ function myColorFunction() {
+    document.getElementById("background").style.backgroundColor = "rgb(211, 84, 49);";
+  }  
+
+
